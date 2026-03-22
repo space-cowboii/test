3ee5aefc372688cd70303b95c88138a1fd98fc12
Ruslan <r.mamedov1703@mail.ru>
2026-03-22 17:42:22 +0300

    Хз какой коммит

---
+++ Project4/script.js
@@ -0,0 +1,25 @@
+function first(){
+    setTimeout( function() {
+        console.log(1);
+    }, 500);
+}
+
+function second() {
+    console.log(2);
+}
+
+first();
+second();
+
+function learnJS(lang, callback) {
+    console.log("Я учу " + lang);
+    callback();
+}
+
+function done() {
+    console.log("Пройден 3-й урок");
+}
+
+learnJS("Javascript", done);
+
+
